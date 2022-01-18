b9437a82b2b9cfa77476bb8c6ef79afd1154e873
rahman-anika <anikarahman37@gmail.com>
2022-01-18 13:59:53 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const winter = document.getElementById('winter');
+console.log(winter.innerText);
+
+
+document.getElementById('buy-now-first').addEventListener('click', function () {
+    const penguin = document.getElementById('penguin');
+    penguin.innerText = 'I AM PENGUIN';
+
+});
+
+
